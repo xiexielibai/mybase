642deec0887aa85b7c214e2ca3749f751f2000d6
baby <baby@192.168.2.6>
2022-11-27 22:16:52 +0800

    add

---
+++ large.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+
+TARGET = "/Users/baby/Downloads/basesoft"
+
+map = {
+
+    "Python-3.11.0.tar.xz": ("python3", "sh configure --prefix=/usr/local/python --enable-optimizations", False, True),
+
+    "mysql-boost-5.7.39.tar.gz": ("mysql",
+                                  f"cmake -DCMAKE_INSTALL_PREFIX=/usr/local/mysql -DMYSQL_UNIX_ADDR=/tmp/mysql.sock -DDEFAULT_CHARSET=utf8 -DDEFAULT_COLLATION=utf8_general_ci -DWITH_INNOBASE_STORAGE_ENGINE=1-DWITH_ARCHIVE_STORAGE_ENGINE=1 -DWITH_BLACKHOLE_STORAGE_ENGINE=1 -DWITH_PARTITION_STORAGE_ENGINE=1 -DENABLED_LOCAL_INFILE=1 -DMYSQL_USER=_mysql -DMYSQL_TCP_PORT=3306 -DMYSQL_DATADIR=/usr/local/mysql/db_data -DDOWNLOAD_BOOST=1 -DWITH_BOOST={TARGET}/mysql/mysql-5.7.39/boost",
+                                  False, True),
+    "amazon-corretto-8.352.08.1-macosx-x64.tar.gz": ("jdk8",
+                                                     "sh configure --with-boot-jdk=../amazon-corretto-8.jdk/Contents/Home --with-freetype-include=/usr/local/include/freetype2 --with-freetype-lib=/usr/local/lib",
+                                                     False, False),
+    "corretto-11-11.0.17.8.1.tar.gz": (
+        "jdk11", "sh configure --with-boot-jdk=../amazon-corretto-11.jdk/Contents/Home", False, False)
+
+}
+
+os.chdir(TARGET)
+
+for key, value in map.items():
+    to = value[0]
+    folder = os.path.join(TARGET, to)
+    if (os.path.exists(folder)):
+        shutil.rmtree(folder)
+    os.system(f"mkdir {to}")
+    os.system(f"tar -zxf {key} -C {to}")
+
+for key, value in map.items():
+    os.chdir(os.path.join(TARGET, value[0], key.replace(".tar.gz", "").replace(".tar.xz", "").replace("boost-", "")))
+    os.system("pwd")
+    if value[1]:
+        os.system("export PATH=/Applications/CMake.app/Contents/bin:$PATH && " + value[1])
+        os.system("make")
+        if value[2]:
+            os.system("sudo -S make install")
---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import shutil
+
+TARGET = "/Users/baby/Downloads/basesoft"
+
+map = {
+    "pkg-config-0.29.2.tar.gz": ("pkg-config", "sh configure --with-internal-glib", False),
+    "redis-7.0.5.tar.gz": ("redis", False, True),
+    "lua-5.4.4.tar.gz": ("lua", False, True),
+    "autoconf-2.69.tar.gz": ("autoconf", "sh configure", False),
+    "freetype-2.12.1.tar.gz": ("freetype", "sh configure", False),
+    "openssl-1.1.1s.tar.gz": ("openssl", "./config", False)
+}
+
+os.chdir("/usr/local")
+os.system("sudo -S rm -rf *")
+
+os.chdir(TARGET)
+
+for key, value in map.items():
+    to = value[0]
+    folder = os.path.join(TARGET, to)
+    if (os.path.exists(folder)):
+        shutil.rmtree(folder)
+    os.system(f"mkdir {to}")
+    os.system(f"tar -zxf {key} -C {to}")
+
+for key, value in map.items():
+    os.chdir(os.path.join(TARGET, value[0], key.replace(".tar.gz", "")))
+    os.system("pwd")
+    if value[1]:
+        print(value[1])
+        os.system(value[1])
+        os.system("make")
+        os.system("sudo -S make install")
+    else:
+        os.system("make")
+        if value[2]:
+            os.system("make test")
