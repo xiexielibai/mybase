88061dc566eeabfd1bf715856575d756c7d3736c
keke <keke@192.168.2.8>
2022-12-04 00:02:50 +0800

    add

--- check.py
+++ check.py
@@ -1,20 +1,27 @@
 import os
 
-check = ["bin/autoconf", "bin/pkg-config", "bin/openssl", "include/freetype2", "python/bin/python3", "mysql/bin/mysqld"]
+BASE = "/Users/baby/Downloads/basesoft"
+
+check = ["bin/autoconf",
+         "bin/pkg-config",
+         "bin/openssl",
+         "include/freetype2",
+         "python/bin/python3",
+         "mysql/bin/mysqld"]
+
 for f in check:
     t = os.path.join("/usr/local", f)
 if not os.path.exists(t):
     raise IOError("not find")
 
 if not os.path.exists(
-        "/Users/baby/Downloads/basesoft/jdk8/corretto-8-8.352.08.1/build/macosx-x86_64-normal-server-release/jdk/bin/java"):
+        f"{BASE}/jdk8/corretto-8-8.352.08.1/build/macosx-x86_64-normal-server-release/jdk/bin/java"):
     raise IOError("not find")
 
 if not os.path.exists(
-        "/Users/baby/Downloads/basesoft/jdk11/corretto-11-11.0.17.8.1/build/macosx-x86_64-normal-server-release/jdk/bin/java"):
+        f"{BASE}/jdk11/corretto-11-11.0.17.8.1/build/macosx-x86_64-normal-server-release/jdk/bin/java"):
     raise IOError("not find")
 
-
 if not os.path.exists(
-        "/Users/baby/Downloads/basesoft/jdk17/corretto-17-17.0.5.8.1/build/macosx-x86_64-server-release/jdk/bin/java"):
+        f"{BASE}/jdk17/corretto-17-17.0.5.8.1/build/macosx-x86_64-server-release/jdk/bin/java"):
     raise IOError("not find")
